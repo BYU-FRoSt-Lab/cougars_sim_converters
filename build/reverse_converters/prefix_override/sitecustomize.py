23cd7ddefbb60f7747cdc3d17928547f649a9bb6
Matthew McMurray <mmcmurray123@gmail.com>
2025-01-16 16:45:28 -0700

    deleted dumb directories

--- build/reverse_converters/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mmcmurray/drew_stuff/CoUGARs/cougars_sim_converters/install/reverse_converters'
